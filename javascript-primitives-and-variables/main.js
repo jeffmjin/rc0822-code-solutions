330f6c3d823840ba8ffb4c54650ce92924452b0a
Jeff Jin <jeffmjin@gmail.com>
2022-08-29 16:30:19 +0000

    Completed javascript-primitives-and-variables exercise.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,21 @@
+var fullName = 'Jeff Jin';
+var isCool = true;
+var totalPets = 0;
+
+console.log('value of fullName:', fullName);
+console.log('value of isCool:', isCool);
+console.log('value of totalPets:', totalPets);
+
+console.log('typeof fullName:', typeof 'Jeff Jin');
+console.log('typeof isCool:', typeof true);
+console.log('typeof totalPets:', typeof 0);
+
+var unknown = null;
+
+console.log('value of unknown:', unknown);
+console.log('typeof unknown:', typeof unknown);
+
+var never;
+
+console.log('value of never:', never);
+console.log('typeof never:', typeof never);
