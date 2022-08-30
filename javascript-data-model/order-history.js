462b046a001655557c74453d4e03b197d05872f8
Jeff Jin <jeffmjin@gmail.com>
2022-08-30 16:42:54 +0000

    Completed javascript-data-model exercise.

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,52 @@
+var orderHistory = [
+  {
+    orderNumber: '114-3941689-8772232',
+    orderDate: 'August 4, 2020',
+    deliveryDate: 'August 8, 2020',
+    type: 'book',
+    title: 'JavaScript for impatient programmers',
+    author: 'Dr. Axel Rauschmayer',
+    price: '$31.55',
+    total: '$34.00',
+    ship: 'JS Masher',
+    returnWindow: 'September 7, 2020'
+  },
+  {
+    orderNumber: '113-9984268-1280257',
+    orderDate: 'July 19, 2020',
+    deliveryDate: 'July 20, 2020',
+    type: 'book',
+    title: 'The Timeless Way of Building',
+    author: 'Christopher Alexander',
+    price: '$41.33',
+    total: '$44.53',
+    ship: 'JS Masher',
+    returnWindow: 'August 19, 2020'
+  },
+  {
+    orderNumber: '114-2875557-9059409',
+    orderDate: 'July 4, 2020',
+    deliveryDate: 'July 7, 2020',
+    type: 'Controller Adapter',
+    title: 'Gamecube Controller Adapter: Super Smash bros Switch Gamecube Adapter for for WII U',
+    price: '$15.98',
+    total: '$17.22',
+    ship: 'JS Masher',
+    returnWindow: 'August 5, 2020'
+  },
+  {
+    orderNumber: '113-2883177-2648248',
+    orderDate: 'July 3, 2020',
+    deliveryDate: 'July 5, 2020',
+    type: 'Game Controller',
+    title: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+    price: '$96.95',
+    type_2: 'book',
+    title_2: 'The Art of Sql',
+    author: 'Stephane Faroult',
+    price_2: '$33.99',
+    total: '$138.93',
+    ship: 'JS Masher',
+    returnWindow: 'August 4, 2020'
+  }
+];
